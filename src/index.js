7a0017f2d9e0f0b6a690626c12fdaa0e8417e73d
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-08-14 17:17:25 +0200

    excercise 24

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store/index.js';
+import routes from './routes';
+
 
 import { getCountries } from './actions/actions-countries'
 
@@ -9,10 +12,13 @@ import DevTools from './DevTools';
 
 render (
 	<Provider store = {store}>
-		<div>
-			<h1>Inicjalizacja projektu</h1>
-			<DevTools />
-		</div>
+		<Router history={hashHistory} routes={routes} />
+				{ /* <div>
+					<h1>Inicjalizacja projektu</h1>
+					<DevTools />
+				</div> -
+			  */}
+
 	</Provider>,
 	document.getElementById('root')
 );
